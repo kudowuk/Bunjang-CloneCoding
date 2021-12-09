4d0504a395a767680b4a7a597d04899bafb40233
kudowuk <knock06@naver.com>
2021-12-09 23:42:27 +0900

    2021-12-09 USER, STORE commit

--- src/main/java/com/example/demo/src/category/model/CategoryVo.java
+++ src/main/java/com/example/demo/src/category/model/CategoryVo.java
@@ -1,14 +1,2 @@
-package com.example.demo.src.product.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class CategoryVo {
-
-    private int categoryIdx;
-    private String categoryName;
+package com.example.demo.src.category.model;public class CategoryVo {
 }
